a126ea8efbf4a173de6b7901e56e5a0dc493482e
Sam James <samjojames@gmail.com>
2025-01-10 16:48:37 +0000

    firebase auth, context, login page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { Nav } from "./components/Nav.tsx";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 import { TaskList } from "./pages/TaskList.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ChakraProvider } from "@chakra-ui/react";
 import { defaultSystem } from "@chakra-ui/react";
+import { AuthProvider } from "./auth/authContext.tsx";
+import { SignIn } from "./auth/SignIn.tsx";
 
 const queryClient = new QueryClient();
 
@@ -16,13 +18,17 @@ createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={queryClient}>
       <ChakraProvider value={defaultSystem}>
         <BrowserRouter>
-          <main className="bg-gray-100 dark:text-white dark:bg-gray-900 min-h-screen font-mono w-full">
-            <Nav />
-            <Routes>
-              <Route path="/" element={<App />} />
-              <Route path="/tasks" element={<TaskList />} />
-            </Routes>
-          </main>
+          <AuthProvider>
+            <main className="bg-gray-100 dark:text-white dark:bg-gray-900 min-h-screen font-mono w-full">
+              <Nav />
+              <Routes>
+                <Route path="/" element={<App />} />
+                <Route path="/signin" element={<SignIn />} />
+                <Route path="/dashboard" element={<Navigate to="/" />} />
+                <Route path="/tasks" element={<TaskList />} />
+              </Routes>
+            </main>
+          </AuthProvider>
         </BrowserRouter>
       </ChakraProvider>
     </QueryClientProvider>
