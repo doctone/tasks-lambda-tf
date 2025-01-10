7b2770268c41ed7e32ab37e4c9a575ef890cc674
Sam James <samjojames@gmail.com>
2025-01-10 17:23:19 +0000

    gitignore

--- frontend/src/auth/firebase.ts
+++ frontend/src/auth/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const app = initializeApp({
-  apiKey: "AIzaSyBq2opKCe3SecLcDRkeDbng1YWZO8eFMZs",
-  authDomain: "task-manager-a6daf.firebaseapp.com",
-  projectId: "task-manager-a6daf",
-  storageBucket: "task-manager-a6daf.firebasestorage.app",
-  messagingSenderId: "1070943901021",
-  appId: "1:1070943901021:web:a765b7766de120f03a44b4",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 });
 
 export const firebaseAuth = getAuth(app);
